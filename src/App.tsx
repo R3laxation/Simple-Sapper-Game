18b73268c123898f43b0cf78974ba66c2d6aab69
R3laxation <memories@bk.ru>
2023-02-10 17:49:44 +0600

    create enum Mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {createField, Mine} from "./helpers/createField/createField";
 
+enum Mask {
+    Transparent,
+    Fill,
+    Flag,
+    Question
+}
 
 function App() {
-  return (
-    <div >
-        123
-    </div>
-  );
+
+    const size = 10;
+    const dimension = new Array(size).fill(null);
+    const [field, setField] = useState<number[]>(() => createField(size));
+    const [mask, setMask] = useState<number[]>(() => new Array(size * size).fill(Mask.Fill));
+
+
+    return (
+        <div>
+            {dimension.map((_, y) => {
+                return (
+                    <div key={y} style={{display: 'flex'}}>
+                        {dimension.map((_, x) => {
+                            return (
+                                <div key={x} style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    width: 24,
+                                    height: 24,
+                                    margin: 1,
+                                    backgroundColor: '#BEB'
+                                }}>
+                                    {
+                                        mask[y * size + x] !== Mask.Transparent
+                                            ? mask
+                                            : field[y * size + x] === Mine
+                                                ? <span>&#128163;</span>
+                                                : field[y * size + x]
+                                    }
+
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
--- src/helpers/createField/createField.ts
+++ src/helpers/createField/createField.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Mine = -1;
+export const Mine = -1;
 
 export const createField = (size: number): number[] => {
 
